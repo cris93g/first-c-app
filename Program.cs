bffba558f027b32ed42c3676f6fea326861b4ff3
cris93g <37388115+cris93g@users.noreply.github.com>
2019-04-12 18:41:58 -0500

    first app

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace myapp
+{
+    class Program
+    {
+        static void Main()
+        {
+            //prompt the user for minustes exercised
+            Console.Write("Enter how many minutes you exercised?");
+
+            string entry = Console.ReadLine();
+
+
+
+
+
+            //add minutes exercised to total
+            //display total munutes to the screen
+            Console.WriteLine("you've entered " + entry + " minutes");
+            //repeat until user quits
+
+        }
+    }
+}
